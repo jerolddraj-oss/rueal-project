631f88cc5d5813f33c0da92dcb740b5b63c01e7d
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-09-19 07:57:04 +0530

    security: harden modules/security/main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,20 +1,20 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "this" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
   location = var.location
-  tags = var.tags
+  tags     = var.tags
 }
 
 resource "azurerm_key_vault" "this" {
-  name = var.name_prefix
-  location = var.location
-  resource_group_name = azurerm_resource_group.this.name
-  tenant_id = var.tenant_id
-  sku_name = "standard"
-  purge_protection_enabled = true
-  soft_delete_retention_days = 90
-  enable_rbac_authorization = true
-  public_network_access_enabled = true
-  tags = var.tags
+  name                          = var.name_prefix
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.this.name
+  tenant_id                     = var.tenant_id
+  sku_name                      = "standard"
+  purge_protection_enabled      = true
+  soft_delete_retention_days    = 90
+  enable_rbac_authorization     = true
+  public_network_access_enabled = false
+  tags                          = var.tags
 }
